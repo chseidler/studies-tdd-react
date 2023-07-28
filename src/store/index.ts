ee832ff549743c9689266e7dbfe3c2f17ad41db7
Christian Seidler <ch.cseidler@gmail.com>
2023-07-27 21:12:08 -0300

    axios and api store created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
 import {applyMiddleware, legacy_createStore} from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
+import api from '../api';
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+const store = legacy_createStore(
+  rootReducer,
+  applyMiddleware(thunk.withExtraArgument(api)),
+);
 
 export default store;
