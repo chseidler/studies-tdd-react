1d013da678bb5f8dab27423a12bc92eef8de168d
Christian Seidler <ch.cseidler@gmail.com>
2023-07-28 21:54:26 -0300

    first style to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 import {Provider} from 'react-redux';
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from '@mui/material';
+import {green} from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: green,
+  },
+});
 
 export default function App() {
   return (
     <Provider store={store}>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Opinion Ate</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <RestaurantScreen />
+        </Container>
+      </ThemeProvider>
     </Provider>
   );
 }
