3064aae07e050fb412e430b4cb34a2211470627c
Christian Seidler <ch.cseidler@gmail.com>
2023-08-04 22:25:22 -0300

    Added button to add new restaurant

--- src/components/NewRestaurantForm.tsx
+++ src/components/NewRestaurantForm.tsx
@@ -1,9 +1,12 @@
-import {TextField} from '@mui/material';
+import {Button, TextField} from '@mui/material';
 
 export function NewRestaurantForm() {
   return (
     <form>
       <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
+      <Button variant="contained" color="primary">
+        Add
+      </Button>
     </form>
   );
 }
