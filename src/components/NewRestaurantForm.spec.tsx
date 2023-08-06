11d5bbdbe9eaef21abfe4fba4105529afd037bd7
Christian Seidler <ch.cseidler@gmail.com>
2023-08-05 21:57:52 -0300

    form is filled in test

---
+++ src/components/NewRestaurantForm.spec.tsx
@@ -0,0 +1,32 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NewRestaurantForm from './NewRestaurantForm';
+
+describe('NewRestaurantForm', () => {
+  const restaurantName = 'Sushi Place';
+
+  let user: any;
+  let createRestaurant: any;
+
+  function renderComponent() {
+    user = userEvent.setup();
+    createRestaurant = jest.fn().mockName('createRestaurant');
+    render(<NewRestaurantForm createRestaurant={createRestaurant} />);
+  }
+
+  describe('when filled in', () => {
+    async function fillInForm() {
+      renderComponent();
+      await user.type(
+        screen.getByPlaceholderText('Ádd Restaurant'),
+        restaurantName,
+      );
+      await user.click(screen.getByText('Add'));
+    }
+
+    it('calls createRestaurant with the name', async () => {
+      await fillInForm();
+      expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
+    });
+  });
+});
