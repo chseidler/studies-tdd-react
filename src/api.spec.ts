ef3ddeb3c952f9504968c711768b223855485571
Christian Seidler <ch.cseidler@gmail.com>
2023-08-09 22:33:20 -0300

    nock to mock the api response

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -1,10 +1,16 @@
+import nock from 'nock';
 import api from './api';
 
 describe('api', () => {
   describe('loadRestaurants', () => {
+    const responseHeaders = {'Access-Control-Allow-Origin': '*'};
     const restaurants = [{id: 1, name: 'Sushi Place'}];
 
     it('returns the response to the right endpoint', async () => {
+      nock('https://api.outsidein.dev')
+        .get(/^\/\w+\/restaurants$/)
+        .reply(200, restaurants, responseHeaders);
+
       await expect(api.loadRestaurants()).resolves.toEqual(restaurants);
     });
   });
