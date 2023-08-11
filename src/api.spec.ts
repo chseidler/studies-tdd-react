37e4db3ccc0bebbbaa60e394f52d9672b36000d4
Christian Seidler <ch.cseidler@gmail.com>
2023-08-10 22:23:42 -0300

    test for the api.createRestaurant() method

--- src/api.spec.ts
+++ src/api.spec.ts
@@ -2,8 +2,9 @@ import nock from 'nock';
 import api from './api';
 
 describe('api', () => {
+  const responseHeaders = {'Access-Control-Allow-Origin': '*'};
+
   describe('loadRestaurants', () => {
-    const responseHeaders = {'Access-Control-Allow-Origin': '*'};
     const restaurants = [{id: 1, name: 'Sushi Place'}];
 
     it('returns the response to the right endpoint', async () => {
@@ -12,6 +13,21 @@ describe('api', () => {
         .reply(200, restaurants, responseHeaders);
 
       await expect(api.loadRestaurants()).resolves.toEqual(restaurants);
+    });
+  });
+
+  describe('createRestaurant', () => {
+    const restaurantName = 'Sushi Place';
+    const responseResraurant = {id: 1, name: restaurantName};
+
+    it('returns the response to the right endpoint', async () => {
+      nock('https://api.outsidein.dev')
+        .post(/^\/\w+\/restaurants$/, {name: restaurantName})
+        .reply(200, responseResraurant, responseHeaders);
+
+      await expect(api.createRestaurant(restaurantName)).resolves.toEqual(
+        responseResraurant,
+      );
     });
   });
 });
